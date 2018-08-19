91c785af1a18fa3633f12be32f753871af1dc52d
tarzanChan <tarzan_Chan@outlook.com>
2018-08-19 17:00:21 +0800

    周分享_shell清除日志

---
+++ better_clean.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+LOG_DIR=/home/guoshare/
+ROOT_UID=0
+if [ "$UID" -ne $ROOT_UID ]
+then
+    echo 'must be root to run this script'
+    exit 1
+fi
+
+cd $LOG_DIR || {
+    echo 'Cannot change to necessary directory.'>&2
+    exit 1
+}
+
+cat /dev/null > helloworld.txt && echo "Logs cleaned up"
+exit 0
---
+++ clean.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+cd /home/guoshare/
+cat /dev/null > helloworld.txt
+echo "logs cleans up."
+
